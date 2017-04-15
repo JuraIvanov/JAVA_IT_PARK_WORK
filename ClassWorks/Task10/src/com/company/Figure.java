c53563886ce223feeefff92ada6d779536e3e8d5
JuraIvanov <seamangt123@gmail.com>
2017-04-15 20:08:59 +0300

    123

---
+++ ClassWorks/Task10/src/com/company/Figure.java
@@ -0,0 +1,16 @@
+package com.company;
+
+/**
+ * Created by Student10 on 15.04.2017.
+ */
+public class Figure {
+    protected String name;
+
+    public Figure(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
